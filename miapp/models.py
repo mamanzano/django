6c30abf6ae5f09aad8022590c3eff2e0d8d67907
mamanzano <tesidhc@gmail.com>
2022-08-17 00:18:06 -0500

    models

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,17 @@
+from turtle import title
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    public = models.BooleanField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    descrition = models.CharField(max_length=250)
+    created_at = models.DateTimeField()
